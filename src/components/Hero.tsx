3caaa26f0cf785500b96270b2907fbed7dca033c
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-07-05 07:12:41 -0700

    add animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,12 +8,14 @@ const Hero = () => {
     return (
         <div className='hero flex max-w-[1200px] mx-auto h-[100vh] items-start'>
             <div className="font-WorkSans w-1/2 pt-16">
-                <h4 className="text-xl text-red-400 font-semibold" data-aos="fade-right"  data-aos-duration="500" >COMPANY</h4>
-                <h2 className="text-6xl font-semibold" data-aos="fade-right"  data-aos-duration="1200" >Creative <br /><span className="text-green-500">Marketing</span></h2>
-                <p className="text-primary-50 max-w-[500px] mt-5" data-aos="fade-right"  data-aos-duration="2000">
+                <h4 className="text-xl text-red-400 font-semibold" data-aos="fade-right" data-aos-duration="500" >COMPANY</h4>
+                <h2 className="text-6xl font-semibold" data-aos="fade-right" data-aos-duration="1200" >Creative <br /><span className="text-green-500">Marketing</span></h2>
+                <p className="text-primary-50 max-w-[500px] mt-5" data-aos="fade-right" data-aos-duration="2000">
                     The sky was cloudless and of a deep dark blue the spectacle before us was indeed sublime.
                 </p>
-                <a className="btn btn-accent text-white mt-10" data-aos="zoom-in"> Explore</a>
+                <div data-aos="fade-right" data-aos-duration="2200">
+                    <p className="btn btn-accent text-white mt-10" > Explore</p>
+                </div>
             </div>
             <div className="w-1/2 relative z-10">
                 <img className="absolute top-[300px] right-0" src={Rectangle} alt="icon woman" data-aos="fade-up-left" />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,18 @@ const Navbar = () => {
     return (
         <div className="max-w-[1200px] mx-auto flex justify-between items-center py-4 z-10 relative">
             <ul className="flex gap-2 items-center text-primary-50 font-medium">
-                <li className="text-3xl font-bold">CMP</li>
-                <li>About</li>
-                <li>Portfolio</li>
-                <li>Service</li>
-                <li>Blog</li>
-                <li>Contact</li>
+                <li className="text-3xl font-bold" data-aos="fade-right" data-aos-duration="200">CMP</li>
+                <li data-aos="fade-right" data-aos-duration="500">About</li>
+                <li data-aos="fade-right" data-aos-duration="700">Portfolio</li>
+                <li data-aos="fade-right" data-aos-duration="900">Service</li>
+                <li data-aos="fade-right" data-aos-duration="1100">Blog</li>
+                <li data-aos="fade-right" data-aos-duration="1300">Contact</li>
             </ul>
-            <button className="btn btn-secondary btn-outline">
-                Contact
-            </button>
+            <div data-aos="fade-left" data-aos-duration="1300">
+                <button className="btn btn-secondary btn-outline" >
+                    Contact
+                </button>
+            </div>
         </div>
     )
 }
